b722565ce78629d7c40156b099d940a6c2b31e3a
CPWaffenDiscordNew <redbedheadboy@outlook.com>
2026-02-11 05:29:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,63 +1,70 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static('public'));
 
-const voicePeers = {}; // { channelName: { socketId: peerInfo } }
+const messages = {}; // { roomName: [ {username,text,profilePic} ] }
+const voiceChannels = {}; // { channelName: [{username, profilePic}] }
 
+// --- Socket.IO events ---
 io.on('connection', socket => {
   console.log('User connected:', socket.id);
 
-  socket.on('join-room', ({ room, username }) => {
+  // TEXT CHAT
+  socket.on('join-room', ({ room, username, profilePic }) => {
     socket.join(room);
-    socket.room = room;
-    socket.username = username;
-    socket.emit('load-messages', []); // empty or implement persistent storage
+    if (!messages[room]) messages[room] = [];
+    socket.emit('load-messages', messages[room]);
+    console.log(`${username} joined room: ${room}`);
   });
 
-  socket.on('message', data => {
-    io.to(data.room).emit('message', data);
+  socket.on('message', ({ room, text, username, profilePic }) => {
+    const msg = { username, text, profilePic };
+    if (!messages[room]) messages[room] = [];
+    messages[room].push(msg);
+    io.to(room).emit('message', msg);
   });
 
-  // ---------------- Voice chat signaling ----------------
-  socket.on('voice-join', ({ channel, username }) => {
-    socket.channel = channel;
-    socket.username = username;
+  // VOICE CHAT
+  socket.on('voice-join', ({ channel, username, profilePic }) => {
     socket.join(channel);
-
-    if (!voicePeers[channel]) voicePeers[channel] = {};
-    voicePeers[channel][socket.id] = socket;
-
-    // Notify existing peers
-    for (let peerId in voicePeers[channel]) {
-      if (peerId !== socket.id) {
-        socket.emit('new-peer', peerId);
-        voicePeers[channel][peerId].emit('new-peer', socket.id);
-      }
-    }
+    if (!voiceChannels[channel]) voiceChannels[channel] = [];
+    voiceChannels[channel].push({ username, profilePic, id: socket.id });
+    io.emit('voice-users', { channel, users: voiceChannels[channel] });
+    socket.broadcast.emit('new-peer', socket.id);
   });
 
+  socket.on('voice-leave', ({ channel, username }) => {
+    if (voiceChannels[channel]) {
+      voiceChannels[channel] = voiceChannels[channel].filter(u => u.id !== socket.id);
+      io.emit('voice-users', { channel, users: voiceChannels[channel] });
+    }
+    socket.broadcast.emit('peer-left', socket.id);
+  });
+
+  // WebRTC signals
   socket.on('signal', ({ to, signal }) => {
     io.to(to).emit('signal', { from: socket.id, signal });
   });
 
-  socket.on('voice-leave', ({ channel }) => {
-    if (voicePeers[channel]) {
-      delete voicePeers[channel][socket.id];
-      socket.leave(channel);
-      io.to(channel).emit('peer-left', socket.id);
-    }
-  });
-
   socket.on('disconnect', () => {
-    const channel = socket.channel;
-    if (channel && voicePeers[channel]) {
-      delete voicePeers[channel][socket.id];
-      io.to(channel).emit('peer-left', socket.id);
-    }
+    console.log('User disconnected:', socket.id);
+    // Remove user from all voice channels
+    Object.keys(voiceChannels).forEach(channel => {
+      voiceChannels[channel] = voiceChannels[channel].filter(u => u.id !== socket.id);
+      io.emit('voice-users', { channel, users: voiceChannels[channel] });
+      socket.broadcast.emit('peer-left', socket.id);
+    });
   });
 });
 
-http.listen(3000, () => console.log('Server running on port 3000'));
+// --- Start server ---
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
